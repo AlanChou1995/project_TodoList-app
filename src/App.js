a0f317485290ee52ba420046e286227b30b29b24
AlanChou <alanchou19950414@gmail.com>
2022-09-14 21:05:38 +0800

    add localstorage

--- src/App.js
+++ src/App.js
@@ -1,69 +1,80 @@
 import React from 'react';
 import "./App.css"
 import {
-  Container,
-  TodoCard,
-  Title,
-  UserBox,
-  TodoInput,
-  AddButton,
+    Container,
+    TodoCard,
+    Title,
+    UserBox,
+    TodoInput,
+    AddButton,
 } from './components/styled';
-import { useState, useRef } from 'react';
-import TodoItem from './components/TodoItem';
+import { useState, useRef, useEffect } from 'react'
+import TodoItem from './components/TodoItem'
+
+const localStorageTodos = todos => localStorage.setItem('todos', JSON.stringify(todos));
 
 const App = () => {
-const [inputValue, setInputValue] = useState('');
-const [todos, setTodos] = useState([{ id: 1, inputValue: 'Todo...', isDone: false }]);
-const id = useRef(2)
-const handleInputChange = (even) => {
-  setInputValue(even.target.value);
-}
-const handleAddClick = () => {
-  if (inputValue === '') {
-    alert('Please add todo')
-  } else {
-    setTodos([{
-      id: id.current,
-      inputValue: inputValue,
-    }, ...todos,])
-    setInputValue('');
-    id.current++;
-  }
-  console.log(todos)
-}
-const handleDeleteClick = id => {
-  setTodos(todos.filter(todo => todo.id !== id))
-}
-const handleStateClick = (id) => {
-  setTodos(todos.map(todo => {
-    if (todo.id !== id) return todo;
-    return {
-      ...todo,
-      isDone: !todo.isDone
+    const [inputValue, setInputValue] = useState('');
+    const [todos, setTodos] = useState([{ id: 1, inputValue: 'Todo...', isDone: false }]);
+    const id = useRef(2);
+    useEffect(() => {
+        const localData = localStorage.getItem('todos') || '';
+        if (localData) {
+            setTodos(JSON.parse(localData))
+        };
+    }, []);
+    useEffect(() => {
+        localStorageTodos(todos);
+    }, [todos])
+    const handleInputChange = (even) => {
+        setInputValue
+            (even.target.value);
     }
-  }))
-}
-return (
-  <Container>
-    <TodoCard>
-      <Title>Simple Todo List</Title>
-      <UserBox>
-        <TodoInput placeholder='Text' onChange={handleInputChange} value={inputValue} />
-        <AddButton onClick={handleAddClick}>Add</AddButton>
-      </UserBox>
-      {
-        todos.map(
-          todo =>
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              handleDeleteClick={handleDeleteClick}
-              handleStateClick={handleStateClick}
-            />
-        )
-      }
-    </TodoCard>
-  </Container>
-)
+    const handleAddClick = () => {
+        if (inputValue === '') {
+            alert('Please add todo')
+        } else {
+            setTodos([{
+                id: id.current,
+                inputValue: inputValue,
+            }, ...todos,])
+            setInputValue('');
+            id.current++;
+        }
+    }
+    const handleDeleteClick = id => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+    const handleStateClick = (id) => {
+        setTodos(todos.map(todo => {
+            if (todo.id !== id) return todo;
+            return {
+                ...todo,
+                isDone: !todo.isDone
+            }
+        }))
+    }
+    return (
+        <Container>
+            <TodoCard>
+                <Title>Simple Todo List</Title>
+                <UserBox>
+                    <TodoInput placeholder='Text' onChange={handleInputChange} value={inputValue} />
+                    <AddButton onClick={handleAddClick}>Add</AddButton>
+                </UserBox>
+                {
+                    todos.map(
+                        todo =>
+                            <TodoItem
+                                key={todo.id}
+                                todo={todo}
+                                handleDeleteClick={handleDeleteClick}
+                                handleStateClick={handleStateClick}
+                            />
+                    )
+                }
+            </TodoCard>
+        </Container>
+    )
 }
 export default App
